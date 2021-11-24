c0c024b487cd86d8e4eadaa0c4f5b9d3f2b0521e
AhmedAtefGaber <ahmedatef844.218@gmail.com>
2021-11-24 06:57:09 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import datetime, pymongo
+
+app = Flask(__name__)
+
+timeStamp = datetime.datetime.now().timestamp()
+myclient = pymongo.MongoClient("mongodb://mongo:jPG1KhkyMP@3.109.212.126:27017/")
+dblist = myclient.list_database_names()
+
+if "mydatabase" not in dblist:
+    mydb = myclient["mydatabase"]
+    mycol = mydb["strings"]
+
+@app.route('/')
+def query_string():
+    
+    str1 = request.args.get('str1')
+    str2 = request.args.get('str2')
+    fullStr = str1 + str2
+
+    strings_collection = { 'str1' : str1, 'fullStr' : fullStr, 'timeStamp' : timeStamp }
+    x = mycol.insert_one(strings_collection)
+
+    return '''
+              <h1>The str1 value is: {}</h1>
+              <h1>The str2 value is: {}</h1>
+              <h1>the full string is: {}</h1>'''.format(str1, str2, fullStr)
